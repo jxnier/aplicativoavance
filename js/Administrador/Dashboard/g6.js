4bd2f916e563f60c844203b2e62ab925d92fb5fe
Jonier <davidorozco787@gmail.com>
2024-04-21 23:04:43 -0500

    aaa

--- js/Administrador/Dashboard/g6.js
+++ js/Administrador/Dashboard/g6.js
@@ -1,29 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const grafica6 = document.getElementById("grafica6").getContext("2d");
-  const etiquetas6 = ["Malo ", "Regular", "Bueno", "Muy bueno"];
-  const datosVentas20203 = {
-      label: "Satisfacción por el aplicativo",
-      data: [10, 50, 40, 100],
-      backgroundColor: 'rgba(0, 102, 204, 0.5)',
-      borderColor: 'rgba(0, 51, 102, 1)',
-      borderWidth: 1,
-  };
-  new Chart(grafica6, {
+fetch('http://localhost:3000/pacientes_por_tipo_documento_y_prediccion_ia')
+  .then(response => response.json())
+  .then(data => {
+    // Datos recibidos
+    const resultados = data.resultados;
+
+    // Agrupación de datos por tipo de documento
+    const tiposDocumento = {};
+    resultados.forEach(resultado => {
+      if (!tiposDocumento[resultado.tipo_documento]) {
+        tiposDocumento[resultado.tipo_documento] = {};
+      }
+      tiposDocumento[resultado.tipo_documento][resultado.prediccion_ia] = resultado.cantidad_pacientes;
+    });
+
+    // Configuración del gráfico
+    const ctx = document.getElementById('grafica6').getContext('2d');
+    const myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-          labels: etiquetas6,
-          datasets: [
-              datosVentas20203,
-          ]
+        labels: Object.keys(tiposDocumento),
+        datasets: Object.keys(tiposDocumento['CC']).map(prediccionIA => ({
+          label: prediccionIA,
+          data: Object.values(tiposDocumento).map(obj => obj[prediccionIA] || 0),
+          backgroundColor: getRandomColor(),
+          borderColor: getRandomColor(),
+          borderWidth: 1
+        }))
       },
       options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }],
-          },
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
       }
+    });
+  })
+  .catch(error => {
+    console.error('Error al obtener los datos:', error);
   });
-});
+
+// Función para generar colores aleatorios
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
